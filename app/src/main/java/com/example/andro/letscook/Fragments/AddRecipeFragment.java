1f6ab9faebe8b19919c77b945b920ee7b58af6a7
Himanshu Rawat <rawath54@gmail.com>
2017-09-17 17:14:42 +0530

    Add Recipe Fragment Modified

--- app/src/main/java/com/example/andro/letscook/Fragments/AddRecipeFragment.java
+++ app/src/main/java/com/example/andro/letscook/Fragments/AddRecipeFragment.java
@@ -1,6 +1,13 @@
 package com.example.andro.letscook.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andro.letscook.R;
 
 /**
  * Created by himanshurawat on 15/09/17.
@@ -8,7 +15,12 @@ import android.support.v4.app.Fragment;
 
 public class AddRecipeFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.add_recipe_fragment,container,false);
 
 
-
+        return v;
+    }
 }
