c5815c80ebe111c611138bf91570ff254933457a
Himanshu Rawat <rawath54@gmail.com>
2017-09-27 23:09:06 +0530

    Ingredient Pojo Class Added

--- app/src/main/java/com/example/andro/letscook/PojoClass/Ingredients.java
+++ app/src/main/java/com/example/andro/letscook/PojoClass/Ingredients.java
@@ -5,4 +5,26 @@ package com.example.andro.letscook.PojoClass;
  */
 
 public class Ingredients {
+
+    private String heading;
+    private String ingredient_1;
+    private String ingredient_2;
+    private String ingredient_3;
+    private String ingredient_4;
+    private String ingredient_5;
+    private String ingredient_6;
+    private String ingredient_7;
+    private String ingredient_8;
+    private String ingredient_9;
+    private String ingredient_10;
+    private String ingredient_11;
+    private String ingredient_12;
+    private String ingredient_13;
+    private String ingredient_14;
+    private String ingredient_15;
+
+
+    public Ingredients() {
+    }
+
 }
