28c5b8d9638c764d31bb0be5ea0da789c1098d16
Himanshu Rawat <rawath54@gmail.com>
2017-09-27 23:07:55 +0530

    View Recipes Fragment Added

---
+++ app/src/main/java/com/example/andro/letscook/Fragments/ViewRecipeFragment.java
@@ -0,0 +1,25 @@
+package com.example.andro.letscook.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by himanshurawat on 26/09/17.
+ */
+
+public class ViewRecipeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v;
+
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/andro/letscook/PojoClass/Ingredients.java
@@ -0,0 +1,8 @@
+package com.example.andro.letscook.PojoClass;
+
+/**
+ * Created by himanshurawat on 26/09/17.
+ */
+
+public class Ingredients {
+}
