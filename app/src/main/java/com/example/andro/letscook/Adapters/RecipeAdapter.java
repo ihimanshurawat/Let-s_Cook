c24fedd021c8a2eec4026147ef08f3bab7355a53
Himanshu Rawat <rawath54@gmail.com>
2017-10-01 10:37:30 +0530

    Recipe Adapter Updated

--- app/src/main/java/com/example/andro/letscook/Adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/andro/letscook/Adapters/RecipeAdapter.java
@@ -1,14 +1,20 @@
 package com.example.andro.letscook.Adapters;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.andro.letscook.Fragments.EditProfileFragment;
+import com.example.andro.letscook.Fragments.ViewRecipeFragment;
 import com.example.andro.letscook.PojoClass.Recipe;
 import com.example.andro.letscook.R;
 
@@ -19,14 +25,16 @@ import java.util.List;
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
 
     public Context context;
+    public FragmentManager fragmentManager;
     public List<Recipe> recipeList;
 
 
 
 
-    public RecipeAdapter(Context context,List<Recipe> recipeList){
+    public RecipeAdapter(Context context,List<Recipe> recipeList,FragmentManager fragmentManager){
         this.context=context;
         this.recipeList=recipeList;
+        this.fragmentManager=fragmentManager;
     }
 
     @Override
@@ -54,7 +62,7 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     }
 
 
-    public static class RecipeViewHolder extends RecyclerView.ViewHolder {
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView recipeImageView;
         TextView recipeName,recipeTotalTime,recipeFavourite;
@@ -66,9 +74,27 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
             recipeName=itemView.findViewById(R.id.recipes_fragment_item_view_recipe_name_text_view);
             recipeTotalTime= itemView.findViewById(R.id.recipes_fragment_total_cook_time_text_view);
           //  recipeFavourite=itemView.findViewById(R.id.recipes_fragment_recipe_favourite_text_view);
-
+            itemView.setOnClickListener(this);
 
         }
+
+        @Override
+        public void onClick(View view) {
+            Recipe recipe=recipeList.get(getAdapterPosition());
+            Toast.makeText(context,"Item Clicked",Toast.LENGTH_SHORT).show();
+            launchViewRecipeFragment(recipe);
+
+        }
+    }
+
+    private void launchViewRecipeFragment(Recipe recipe) {
+        ViewRecipeFragment viewRecipeFragment=new ViewRecipeFragment();
+        Bundle bundle=new Bundle();
+        bundle.putSerializable("Recipe",recipe);
+        viewRecipeFragment.setArguments(bundle);
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content_all_recipies_frame_layout,viewRecipeFragment,"View Recipe")
+                .setCustomAnimations(android.R.anim.slide_in_left,android.R.anim.slide_out_right).commit();
     }
 
     public String getTime(int x){
