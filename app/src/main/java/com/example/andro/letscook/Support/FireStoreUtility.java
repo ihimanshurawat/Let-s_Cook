3b4ef69913f0336d0296b22ead9a20042ca648db
Himanshu Rawat <rawath54@gmail.com>
2017-10-07 10:45:22 +0530

    FireStore Utility Class Added

--- app/src/main/java/com/example/andro/letscook/Support/FireStoreUtility.java
+++ app/src/main/java/com/example/andro/letscook/Support/FireStoreUtility.java
@@ -1,8 +1,24 @@
 package com.example.andro.letscook.Support;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
 /**
  * Created by himanshurawat on 05/10/17.
  */
 
 public class FireStoreUtility {
+    private static FirebaseFirestore firebaseFirestore;
+
+    public static FirebaseFirestore getFirebaseFirestore(){
+        if(firebaseFirestore==null){
+            firebaseFirestore=FirebaseFirestore.getInstance();
+            FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                    .setPersistenceEnabled(true)
+                    .build();
+            firebaseFirestore.setFirestoreSettings(settings);
+        }
+        return firebaseFirestore;
+    }
+
 }
